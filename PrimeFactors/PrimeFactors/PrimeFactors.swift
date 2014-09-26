92747d073451b716c8125d46d9ba53dc994b977c
bspatafora <benjamin.spatafora@gmail.com>
2014-09-25 20:50:57 -0500

    Do kata

--- PrimeFactors/PrimeFactors/PrimeFactors.swift
+++ PrimeFactors/PrimeFactors/PrimeFactors.swift
@@ -1,3 +1,10 @@
 func factor(num: Int) -> [Int] {
-  return []
+    var number = num
+    var primes: [Int] = []
+    for var candidate = 2; number > 1; candidate++ {
+        for ; number%candidate == 0; number /= candidate {
+            primes.append(candidate)
+        }
+    }
+    return primes
 }
